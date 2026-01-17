b42edfc496cae78abef4a8df8036a1f6973adeb4
Pedro Scharrff <pedroscharrff@hotmail.com>
2026-01-17 15:05:39 -0400

    atualizando server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,6 +14,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.set('trust proxy', 1);
+
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
